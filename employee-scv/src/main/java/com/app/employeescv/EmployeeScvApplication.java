4ac2b5b794ac1b3047bacb228e28a0d6f5924de6
Thang Nguyen <mrwin95@gmail.com>
2022-08-14 10:41:26 -0600

    convert main project to pom project

---
+++ employee-scv/src/main/java/com/app/employeescv/EmployeeScvApplication.java
@@ -0,0 +1,13 @@
+package com.app.employeescv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeScvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeScvApplication.class, args);
+    }
+
+}
