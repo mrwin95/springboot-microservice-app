cd73accbb62ea7426d625e1c6fcdcbaf3b1e2adf
Thang Nguyen <mrwin95@gmail.com>
2022-08-14 10:19:20 -0600

    first commit

---
+++ discovery-svc/src/main/java/com/app/discoverysvc/DiscoverySvcApplication.java
@@ -0,0 +1,13 @@
+package com.app.discoverysvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoverySvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoverySvcApplication.class, args);
+    }
+
+}
---
+++ discovery-svc/src/test/java/com/app/discoverysvc/DiscoverySvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.discoverysvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoverySvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
