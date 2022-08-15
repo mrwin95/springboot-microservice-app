ff8a4c2edbc637604690ec7d9086905a198df677
Thang Nguyen <mrwin95@gmail.com>
2022-08-15 16:27:35 -0600

    updated postgresql

--- employee-svc/src/main/java/com/app/employeesvc/EmployeeScvApplication.java
+++ employee-svc/src/main/java/com/app/employeesvc/EmployeeScvApplication.java
@@ -2,8 +2,10 @@ package com.app.employeesvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class EmployeeScvApplication {
 
     public static void main(String[] args) {
