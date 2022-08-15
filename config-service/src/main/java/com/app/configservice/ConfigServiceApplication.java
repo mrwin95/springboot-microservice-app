2feaaacc23888e49b03e5cc43f445edb55895c53
Thang Nguyen <mrwin95@gmail.com>
2022-08-15 16:59:22 -0600

    fixed connect config file server

--- config-service/src/main/java/com/app/configservice/ConfigServiceApplication.java
+++ config-service/src/main/java/com/app/configservice/ConfigServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-@EnableConfigurationProperties
+//@EnableConfigurationProperties
 public class ConfigServiceApplication {
 
     public static void main(String[] args) {
