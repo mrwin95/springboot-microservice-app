4943848bc16dc3c73c94d88f911d1347614b3229
Thang Nguyen <mrwin95@gmail.com>
2022-08-14 19:52:09 -0600

    added config service

---
+++ employee-svc/src/main/java/com/app/employeesvc/EmployeeScvApplication.java
@@ -0,0 +1,13 @@
+package com.app.employeesvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeScvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeScvApplication.class, args);
+    }
+
+}
---
+++ employee-svc/src/test/java/com/app/employeesvc/EmployeeScvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.employeesvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeScvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
